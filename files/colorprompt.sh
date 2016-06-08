a152ec90c1b4d9083a158ddc99723eb7130a4b28
Jason Sherman <jsnshrmn@users.noreply.github.com>
2016-06-08 09:54:38 -0500

    Color beta prompts brown

--- files/colorprompt.sh
+++ files/colorprompt.sh
@@ -6,6 +6,9 @@
 # Color production prompts red
 if  [[ $HOSTNAME = *.prod.* ]]; then
   export PS1='\[\e[1;31m\][\u@\h \W]\$\[\e[0m\] '
+# Color beta prompts brown
+elif  [[ $HOSTNAME = *.beta.* ]]; then
+  export PS1='\[\e[0;33m\][\u@\h \W]\$\[\e[0m\] '
 # Color test prompts yellow
 elif  [[ $HOSTNAME = *.test.* ]]; then
   export PS1='\[\e[1;33m\][\u@\h \W]\$\[\e[0m\] '
