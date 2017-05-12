a6119ce0f2ad8242f066aa2102848148934461db
Jason Sherman <jsn.sherman@gmail.com>
2017-05-12 09:39:17 -0500

    Add wrapper script for checking cpu steal time before running task.

---
+++ files/cpu_steal_check.sh
@@ -0,0 +1,33 @@
+#!/usr/bio/env bash
+
+# Ensures CPU steal time is below specified percentage before executing
+# commands via bash.
+
+if [ -z "$1" ]; then
+  cat <<USAGE
+  Ensures CPU steal time is below specified percentage before executing task.
+  Usage: cpu_steal_check.sh \$steal_percent_threshold \$command [\$command args]
+USAGE
+
+  exit 1;
+fi
+
+if [ -z "$2" ]; then
+  cat <<USAGE
+  Ensures CPU steal time is below specified percentage before executing task.
+  Usage: cpu_steal_check.sh \$steal_percent_threshold \$command [\$command args]
+USAGE
+
+  exit 1;
+fi
+
+read cpu user nice system idle iowait irq softirq steal guest guest_nice< /proc/stat
+total=$(($user+$nice+$system+$idle+$iowait+$irq+$softirq+$steal+$guest+$guest_nice))
+steal_percent=$((100*$steal/$total))
+
+if [[ $steal_percent -gt $1 ]]; then
+  echo "ERROR: CPU steal ${steal_percent}%. Skipping task."
+  exit 1
+fi
+
+bash -c "${@:2}"
